e8802ff286bcadc3eb6ddd3731005fb975824a0a
Darrel Zhang <darrel.zhang@gmail.com>
2020-09-16 01:17:05 -0400

    changed the name to hooslistening

--- hooslistening/asgi.py
+++ hooslistening/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for civconnect project.
+ASGI config for hooslistening project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'civconnect.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hooslistening.settings')
 
 application = get_asgi_application()
--- hooslistening/wsgi.py
+++ hooslistening/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'civconnect.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hooslistening.settings')
 
 application = get_wsgi_application()
