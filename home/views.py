3e36c17dbe5a44db3078cf8a40ba4bcf55b59509
Darrel Zhang <darrel.zhang@gmail.com>
2020-09-23 18:03:15 -0400

    made a templates folder with html files for the homescreen

--- home/views.py
+++ home/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 
-class home(request):
-    
+def home(request):
     return render(request, 'home/homescreen.html')
--- hooslistening/settings.py
+++ hooslistening/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'hooslistening.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
