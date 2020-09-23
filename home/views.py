efbff3632fd90699c548346690fe30143496ecdf
Darrel Zhang <darrel.zhang@gmail.com>
2020-09-23 16:55:20 -0400

    changed views.py to reflect html files

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views import generic
 
 
-class HomeView(generic.DetailView):
-    template_name = 'home/homescreen.html'
+class home(request):
+    
+    return render(request, 'home/homescreen.html')
