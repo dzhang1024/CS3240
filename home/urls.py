822d9e69df6b82d65b3a96cce0a1b8f118d50514
Steven Peng <sjp3cv@virginia.edu>
2020-09-28 23:22:19 -0400

    Updated URLS again
    Fixed HTML extend content

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView
 
 app_name = "home"
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="base.html")),
+    path('', views.home, name='home'),
 ]
