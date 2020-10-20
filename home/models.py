2da641ddb6b6a3fb5fa5288d88b410434c124ed1
Darrel Zhang <darrel.zhang@gmail.com>
2020-10-19 23:18:05 -0400

    added category field to Issues model

--- home/models.py
+++ home/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Issues(models.Models)
     issue_name = models.CharField(max_length=100)
     description = models.TextField()
+    category = models.CharField(max_length=100)
