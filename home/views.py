7485d7484310d37075a781417c9e7f46aa7227c6
Darrel Zhang <darrel.zhang@gmail.com>
2020-10-20 18:55:32 -0400

    changed issues.html and views to list all issues

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
+from .models import Issue 
 
 def home(request):
     return render(request, 'home/homescreen.html')
 
 def issues(request):
-    return render(request, 'issues/issues.html')
+    all_issues = Issue.objects.all()
+    return render(request, 'issues/issues.html', {'issues': all_issues})
