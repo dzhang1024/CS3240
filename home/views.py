f516281239ab5a80915c7199c38bdc39b67ac3e6
ctq3ks <70031942+ctq3ks@users.noreply.github.com>
2020-10-30 13:16:21 -0400

    Revert "Email landing page and model"

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views import generic
+from django.views import generic 
 from django.views.generic.edit import CreateView
-from .models import Issue, Email
+from .models import Issue 
 from django.urls import reverse_lazy
 
 def home(request):
@@ -11,7 +11,7 @@ def home(request):
 
 class IssueList(generic.ListView):
     model = Issue
-    template_name = 'issues/issues.html'
+    template_name = 'issues/issues.html' 
     context_object_name = 'all_issues'
 
 class IssueDetail(generic.DetailView):
@@ -22,11 +22,5 @@ class SubmitIssue(CreateView): #use a createview form to allow users to submit n
     model = Issue
     fields = ['issue_name', 'description', 'category']
     template_name = 'issues/submit_issue.html'
-    success_url = reverse_lazy('home:issues')
-
-class EmailIssue(CreateView):
-    model = Email
-    fields = ['user_name', 'email_comment']
-    template_name = 'issues/email_page.html'
     success_url = reverse_lazy('home:issues')
     
